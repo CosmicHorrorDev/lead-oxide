56d1fb69fd5d86ca84b39969039ed78bcfbfa5be
Lovecraftian Horror <LovecraftianHorror@pm.me>
2020-08-11 13:30:50 -0400

    Don't need any of `ureq`s default features

--- src/constants.rs
+++ src/constants.rs
@@ -1,3 +1,3 @@
-// TODO: should this be https?
+// Note: pubproxy doesn't support https
 pub const API_URI: &str = "http://pubproxy.com/api/proxy?";
 pub const REPO_URI: &str = "https://github.com/LovecraftianHorror/lead-oxide";
