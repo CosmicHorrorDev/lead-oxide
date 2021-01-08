7127a67d3db722a52125983fd161a75cf402b8ac
Lovecraftian Horror <LovecraftianHorror@pm.me>
2021-01-08 01:19:50 -0500

    Cleanup naming

--- src/errors.rs
+++ src/errors.rs
@@ -18,18 +18,18 @@ pub enum ParamError {
 #[derive(Error, Debug)]
 pub enum ApiError {
     // TODO: do all of these really need to have error in their name too? Check other libs
-    #[error("Client Error ({code}): {resp}\n This should be prevented, please raise an issue")]
-    ClientError { code: u16, resp: String },
+    #[error("Client Error ({status}): {text}\n This should be prevented, please raise an issue")]
+    ClientError { status: u16, text: String },
 
-    #[error("Internal Server Error ({code}): {resp}")]
-    ServerError { code: u16, resp: String },
+    #[error("Internal Server Error ({status}): {text}")]
+    ServerError { status: u16, text: String },
 
     #[error("Invalid API key, make sure your key is valid")]
     ApiKeyError,
 
     // TODO: mention fetchers from multiple sessions
     #[error(
-        r"You have exceeded the rate limit. This could be due to multiple programs using the API.
+        "You have exceeded the rate limit. This could be due to multiple programs using the API. \
  If this is not the case then sorry but the API hates you, consider raising an issue."
     )]
     RateLimitError,
@@ -45,26 +45,22 @@ pub enum ApiError {
 }
 
 impl From<NaiveResponse> for ApiError {
-    fn from(resp: NaiveResponse) -> Self {
+    fn from(naive_resp: NaiveResponse) -> Self {
+        let NaiveResponse { status, text } = naive_resp;
+
         // Some known errors get returned with varied `status` codes so match on response text first
         // then add context to unknown status codes
-        match Self::from(resp.text.clone()) {
+        match Self::from(text.clone()) {
             Self::UnknownError => {
-                if resp.status >= 400 && resp.status < 500 {
-                    Self::ClientError {
-                        code: resp.status,
-                        resp: resp.text,
-                    }
-                } else if resp.status >= 500 && resp.status < 600 {
-                    Self::ServerError {
-                        code: resp.status,
-                        resp: resp.text,
-                    }
+                if status >= 400 && status < 500 {
+                    Self::ClientError { status, text }
+                } else if status >= 500 && status < 600 {
+                    Self::ServerError { status, text }
                 } else {
                     unreachable!(
-                        r"Tried creating ApiError from valid response ({}). Please raise an issue
- at {}.",
-                        resp.status,
+                        "Tried creating ApiError from valid response ({}). Please raise an issue \
+                    at {}.",
+                        status,
                         constants::REPO_URI
                     );
                 }
@@ -76,9 +72,9 @@ impl From<NaiveResponse> for ApiError {
 
 const INVALID_API_KEY: &str =
     "Invalid API. Get your API to make unlimited requests at http://pubproxy.com/#premium";
-const RATE_LIMIT: &str = r"We have to temporarily stop you. You're requesting proxies a little too
+const RATE_LIMIT: &str = "We have to temporarily stop you. You're requesting proxies a little too \
  fast (2+ requests per second). Get your API to remove this limit at http://pubproxy.com/#premium";
-const DAILY_LIMIT: &str = r"You have reached the maximum 50 requests for today. Get your API to make
+const DAILY_LIMIT: &str = "You reached the maximum 50 requests for today. Get your API to make \
  unlimited requests at http://pubproxy.com/#premium";
 const NO_PROXY: &str = "No proxy";
 
